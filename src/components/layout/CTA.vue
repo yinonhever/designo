f0eece2e0a8ada265e46668659daa64a0a5c2af0
yinonhever <yinonehever@gmail.com>
2020-11-14 08:50:19 +0200

    adding CTA section

--- src/components/layout/CTA.vue
+++ src/components/layout/CTA.vue
@@ -1,3 +1,38 @@
 <template>
-  <section class="cta"></section>
+  <section class="cta" ref="cta">
+    <img
+      class="cta__bg"
+      src="/assets/shared/desktop/bg-pattern-call-to-action.svg"
+      alt="circles-bg"
+    />
+    <div class="cta__container">
+      <div class="cta__content">
+        <h2 class="heading-2 cta__heading">Let’s talk about your project</h2>
+        <p class="paragraph cta__paragraph">
+          Ready to take it to the next level? Contact us today and find out how
+          our expertise can help your business grow.
+        </p>
+      </div>
+      <a href="/contact">
+        <BaseButton narrow>Get in touch</BaseButton>
+      </a>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const adjustPlacement = () => {
+      const ctaSection = this.$refs.cta;
+      if (window.innerWidth < 600) {
+        ctaSection.style.marginBottom = `-${ctaSection.offsetHeight / 2}px`;
+      } else {
+        ctaSection.style.marginBottom = null;
+      }
+    };
+    adjustPlacement();
+    window.addEventListener("resize", adjustPlacement);
+  },
+};
+</script>
