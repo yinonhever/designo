f7f6849a8124ada0d13525b1c5761c1257085e43
yinonhever <yinonehever@gmail.com>
2020-11-16 00:48:36 +0200

    adding About intro section

--- src/components/layout/PageIntro.vue
+++ src/components/layout/PageIntro.vue
@@ -1,25 +1,44 @@
 <template>
   <section :class="classes">
-    <div class="page-intro__left">
+    <div class="page-intro__content">
       <h1 class="heading-1 page-intro__heading">{{ title }}</h1>
       <p class="paragraph page-intro__paragraph">
-        <slot name="text" />
+        <slot />
       </p>
     </div>
-    <div class="page-intro__right">
-      <slot name="extraContent" />
-    </div>
+    <picture v-if="img">
+      <source
+        :srcset="imgTablet"
+        media="(max-width: 1100px) and (min-width: 501px)"
+      />
+      <source :srcset="imgMobile" media="(max-width: 500px)" />
+      <img class="page-intro__img" :src="img" :alt="title" />
+    </picture>
+    <TheContactForm v-else-if="contact" />
+    <picture v-if="bgImg">
+      <source :srcset="bgImgMobile || bgImg" media="(max-width: 600px)" />
+      <img class="page-intro__bg" :src="bgImg" alt="bg-pattern" />
+    </picture>
   </section>
 </template>
 
 <script>
+import TheContactForm from "../UI/TheContactForm";
+
 export default {
-  props: ["extraClasses", "title"],
+  components: { TheContactForm },
+  props: {
+    title: String,
+    img: String,
+    imgTablet: String,
+    imgMobile: String,
+    bgImg: String,
+    bgImgMobile: String,
+    contact: Boolean,
+  },
   computed: {
     classes() {
-      return this.extraClasses
-        ? "page-intro " + this.extraClasses
-        : "page-intro";
+      return this.contact ? "page-intro page-intro--contact" : "page-intro";
     },
   },
 };
--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,3 +1,25 @@
 <template>
-  <Page title="About"> </Page>
+  <Page title="About">
+    <PageIntro
+      title="About Us"
+      img="/assets/about/desktop/image-about-hero.jpg"
+      imgTablet="/assets/about/tablet/image-about-hero.jpg"
+      imgMobile="/assets/about/mobile/image-about-hero.jpg"
+      bgImg="/assets/about/desktop/bg-pattern-hero-about-desktop.svg"
+      bgImgMobile="/assets/about/mobile/bg-pattern-hero-about-mobile.svg"
+    >
+      Founded in 2010, we are a creative agency that produces lasting results
+      for our clients. We’ve partnered with many startups, corporations, and
+      nonprofits alike to craft designs that make real impact. We’re always
+      looking forward to creating brands, products, and digital experiences that
+      connect with our clients’ audiences.
+    </PageIntro>
+  </Page>
 </template>
+
+<script>
+import PageIntro from "../layout/PageIntro";
+export default {
+  components: { PageIntro },
+};
+</script>
